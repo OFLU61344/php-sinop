21e47283383ffaba4ef6491d76dc6a6b16eeb2eb
ismail3461 <ismail.safa.altun@gmail.com>
2023-03-20 14:05:11 +0300

    Güncel

--- iso2.php
+++ iso2.php
@@ -1,7 +1,24 @@
-<?php
+<?PHP
 
-function selam_ver($msg) {
+include("Fonksiyon.php");
+
+$dosya = "C:/DEBUG/Trabzon";
+$uzantı = ".txt";
+
+
+$deg03 = 0;
+do {
+	echo $deg03++ . " - ".$dosya;
+	$yeni_yol=$dosya.$deg03.$uzantı;
 	
-   echo 'Selam! - '.$msg;
-
-}
+	if (dosyaYaz($yeni_yol,"Sinop")) {
+		echo "Yazma islemi basarili";
+	}
+	else {
+		echo "HATA OLUSTU";
+	}
+	echo "\n";
+} while ($deg03 <= 9
+);   
+	
+?>
